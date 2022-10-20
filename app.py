18c705d4d9d93c04827d03bcb9e19aad3de52d71
sandsot <sandel21@naver.com>
2022-10-20 12:11:04 +0900

    12시

--- app.py
+++ app.py
@@ -6,5 +6,8 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
 if __name__ =='__main__':
     app.run(debug=True,port=80)
